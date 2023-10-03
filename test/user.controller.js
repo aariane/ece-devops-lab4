22d7e368d4c7c283b2e3f075d9df50e03516d76c
aariane <ariane.aubrun@edu.ece.fr>
2023-10-03 12:24:18 +0200

    feat: change branch int he workflow2

--- test/user.controller.js
+++ test/user.controller.js
@@ -30,7 +30,7 @@ describe('User', () => {
 
       }
       userController.create(user, (err, result) => {
-        expect(err).to.be.equal(null)
+        expect(err).to.not.be.equal(null)
         expect(result).to.be.equal('OK')
         done()
       })
